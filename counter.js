2caf6164e7d0c58be65b72285091028f4c7c8d1f
Enes Polat <epenespolat@gmail.com>
2023-02-06 01:05:15 +0300

    Some minimal changes

--- counter.js
+++ counter.js
@@ -1,28 +1,27 @@
-let increase = document.getElementById('btn_inc');
-let reset = document.getElementById('btn_res');
-let decrease = document.getElementById('btn_dec');
+let increase = document.getElementById('btn_inc')
+let reset = document.getElementById('btn_res')
+let decrease = document.getElementById('btn_dec')
 let counter = document.getElementById('counter')
-let count=0;
+let count = 0
 
-
-increase.addEventListener('click', function() {
-  count++;
-  counter.innerHTML = count;
-  if(count > 0){
-    counter.style.color='rgb(10, 109, 55)';
+increase.addEventListener('click', function () {
+  count++
+  counter.innerHTML = count
+  if (count > 0) {
+    counter.style.color = 'rgb(10, 109, 55)'
   }
 })
-reset.addEventListener('click', function() {
-  count = 0;
-  counter.innerHTML = count;
-  if(count == 0){
-    counter.style.color='gray';
+reset.addEventListener('click', function () {
+  count = 0
+  counter.innerHTML = count
+  if (count == 0) {
+    counter.style.color = 'gray'
   }
 })
-decrease.addEventListener('click', function() {
-  count--;
-  counter.innerHTML = count;
-  if(count < 0){
-    counter.style.color='rgb(197, 67, 67)';
+decrease.addEventListener('click', function () {
+  count--
+  counter.innerHTML = count
+  if (count < 0) {
+    counter.style.color = 'rgb(197, 67, 67)'
   }
 })
